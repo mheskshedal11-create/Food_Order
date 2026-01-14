24a6fd62ee81aa36750543f6f3921d67b2a13f74
mahesh kumar chhedal <mheskshedal11@gmail.com>
2026-01-14 18:10:55 +0545

    fix(userschema): remove the user address from user schema

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -34,41 +34,7 @@ const userSchema = mongoose.Schema({
     },
     forgot_date: {
         type: Date,
-    },
-    addresses: [
-        {
-            state: {
-                type: String,
-                required: true
-            },
-            city: {
-                type: String,
-                required: true
-
-            },
-            postalCode: {
-                type: String,
-                required: true
-
-            },
-            location: {
-                type: {
-                    type: String,
-                    enum: ["Point"],
-                    default: "Point"
-
-                },
-                coordinates: {
-                    type: [Number],
-                    required: true
-
-                }, // [lng, lat]
-            },
-            placeName: {
-                type: String
-            }, // <-- store place name from reverse geocoding
-        },
-    ],
+    }
 }, { timestamps: true });
 const User = mongoose.model("User", userSchema);
 export default User;
